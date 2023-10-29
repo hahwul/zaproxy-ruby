a4604951c66df505284dd0ee0300455066a63be8
hahwul <hahwul@gmail.com>
2023-10-30 00:40:25 +0900

    Update test code

--- lib/zap.rb
+++ lib/zap.rb
@@ -4,5 +4,7 @@
 class ZAP
   def initialize
     puts 'ZAP API for Ruby'
+    @endpoint = 'localhost:8080'
+    @apikey = ''
   end
 end
---
+++ spec/zap.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require './lib/zap'
+
+describe ZAP do
+  it 'should do something' do
+    ZAP.new
+  end
+end

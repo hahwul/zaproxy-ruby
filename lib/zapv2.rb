d686bdd9edce8c1813dd3cc252f784a15b2c83b2
hahwul <hahwul@gmail.com>
2023-10-31 23:51:35 +0900

    Add ZAPv2

---
+++ lib/zapv2.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative 'zap'
+
+class ZAPv2 < ZAP
+end
---
+++ spec/zapv2.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require './lib/zapv2'
+
+describe ZAPv2 do
+  it 'initialize default' do
+    ZAPv2.new
+  end
+
+  it 'initialize with endpoint' do
+    ZAPv2.new(endpoint: 'http://localhost:8080')
+  end
+
+  it 'initialize with endpoint and apikey' do
+    ZAPv2.new(endpoint: 'http://localhost:8080', apikey: '1234567890')
+  end
+end

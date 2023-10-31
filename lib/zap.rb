066f2f394bc7cea977fbd5a8d5141b8764ba1071
hahwul <hahwul@gmail.com>
2023-10-31 23:39:55 +0900

    Improve codes

--- lib/zap.rb
+++ lib/zap.rb
@@ -1,9 +1,56 @@
 # frozen_string_literal: true
 
-# ZAP Class
+require 'net/http'
+
 class ZAP
-  def initialize(endpoint: 'http://localhost:8080', apikey: '')
+  API_KEY_DEFAULT = ''
+
+  def initialize(endpoint: 'http://localhost:8080', apikey: API_KEY_DEFAULT)
     @endpoint = endpoint
-    @apikey = apikey
+    @api = API.new(endpoint, apikey)
+  end
+
+  attr_reader :api
+
+  class API
+    def initialize(endpoint, apikey)
+      @url = URI.parse(endpoint)
+      @http = Net::HTTP.new(@url.host, @url.port)
+      @apikey = apikey
+    end
+
+    def get(path)
+      request = build_request(Net::HTTP::Get, path)
+      send_request(request)
+    end
+
+    def post(path, data)
+      request = build_request(Net::HTTP::Post, path)
+      request.set_form_data(data)
+      send_request(request)
+    end
+
+    def delete(path)
+      request = build_request(Net::HTTP::Delete, path)
+      send_request(request)
+    end
+
+    def put(path, data)
+      request = build_request(Net::HTTP::Put, path)
+      request.set_form_data(data)
+      send_request(request)
+    end
+
+    private
+
+    def build_request(request_type, path)
+      request = request_type.new(path)
+      request['X-ZAP-API-Key'] = @apikey unless @apikey.empty?
+      request
+    end
+
+    def send_request(request)
+      @http.request(request)
+    end
   end
 end

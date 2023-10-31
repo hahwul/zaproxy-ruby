27872ed2e2948ba78a73611fed55503f9d3e3b8c
hahwul <hahwul@gmail.com>
2023-10-31 23:13:01 +0900

    Improve testcode

--- spec/zap.rb
+++ spec/zap.rb
@@ -3,7 +3,15 @@
 require './lib/zap'
 
 describe ZAP do
-  it 'should do something' do
+  it 'initialize default' do
     ZAP.new
+  end
+
+  it 'initialize with endpoint' do
+    ZAP.new(endpoint: 'http://localhost:8080')
+  end
+
+  it 'initialize with endpoint and apikey' do
+    ZAP.new(endpoint: 'http://localhost:8080', apikey: '1234567890')
   end
 end
